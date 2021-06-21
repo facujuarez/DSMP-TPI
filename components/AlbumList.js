2ab30cf217504a451c1068518b24f2ad8af2e012
Facundo Juarez <facundo.juarez@gmail.com>
2021-06-20 21:30:35 -0300

    1er commit

---
+++ components/AlbumList.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, FlatList } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+const AlbumList = ({navigation}) =>{
+
+  const [photoset, setPhotoset] = useState(null);
+  
+  useEffect(()=> {
+    axios
+      .get(
+          'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=137290658%40N08&format=json&nojsoncallback=1',
+        )
+        .then((response) => {
+          console.log(response.data.photoset);
+          setPhotoset(response.data.photosets.photoset)
+
+
+        })
+  }, []);
+
+  const renderItem = ({ item }) => (
+      <AlbumDetail
+        navigation={navigation}
+        key={item.id}
+        title={item.title._content}
+        albumId={item.id}
+      />
+  );
+
+  if (!photoset) {
+    return <Text>Loading...</Text>;
+  }
+
+  return (
+    <View style={{flex: 1}}>
+      <FlatList 
+        data={photoset} 
+        renderItem={renderItem}
+      />
+    </View>
+  );
+}
+
+/* class AlbumList extends Component {
+  state = {photoset: null};
+
+  componentWillMount() {
+    axios
+      .get(
+        'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=137290658%40N08&format=json&nojsoncallback=1',
+      )
+      .then((response) =>
+        this.setState({photoset: response.data.photosets.photoset}),
+      );
+  }
+
+  renderAlbums() {
+    return this.state.photoset.map((album) => (
+      <AlbumDetail
+        navigation={this.props.navigation}
+        key={album.id}
+        title={album.title._content}
+        albumId={album.id}
+      />
+    ));
+  }
+
+  render() {
+    console.log(this.state);
+
+    if (!this.state.photoset) {
+      return <Text>Loading...</Text>;
+    }
+
+    return (
+      <View style={{flex: 1}}>
+        <ScrollView>{this.renderAlbums()}</ScrollView>
+      </View>
+    );
+  }
+} */
+
+export default AlbumList;
